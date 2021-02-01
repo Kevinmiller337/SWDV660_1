3c4652644975cb2af471740f9671d08a1ff7819a
Kevinmiller337 <49452339+Kevinmiller337@users.noreply.github.com>
2021-01-31 23:42:58 -0600

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,15 +1,6 @@
-var http = require("http")
-
+var http = require("http");
 http.createServer(function (request, response) {
-
-    // Send the HTTP header
-    // HTTP Status: 200 : OK
-    // Content Type: text/plain
-    response.writeHead(200, { 'Content-Type': 'text/plain' })
-
-    // Send the response body as "Hello Earth."
-    response.end('Hello Earth\n')
-}).listen(3333)
-
-// Console will print the message
-console.log('Server running')
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello Earth\n');
+}).listen(3333);
+console.log('Server running');
